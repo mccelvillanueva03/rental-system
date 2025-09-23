c7996024e92a6979997a5ddc55cc781c968543c9
Mccel Villanueva MacOS <mccel.villanueva1103@gmail.com>
2025-09-24 00:25:27 +0800

    fix(sendEmailOTP): ensure OTP is saved only after successful email sending

--- backend/src/utils/sendEmailOTP.js
+++ backend/src/utils/sendEmailOTP.js
@@ -2,12 +2,7 @@ import transporter from "../config/nodemailer.js";
 
 export async function sendEmailOTP(user) {
   try {
-    const otp = Math.floor(100000 + Math.random() * 900000);
-
-    user.otp = otp;
-    user.otpExpires = Date.now() + 600000;
-
-    await user.save();
+    const otp = Math.floor(100000 + Math.random() * 900000); //generate random 6 digits
 
     const mailOptions = {
       from: process.env.AUTH_EMAIL,
@@ -16,7 +11,16 @@ export async function sendEmailOTP(user) {
       html: `<h1>Verify Your Email</h1><p>Enter this OTP to your app for email verification.<br><strong>${otp}</strong></p>`,
     };
 
-    await transporter.sendMail(mailOptions);
+    const emailSent = await transporter.sendMail(mailOptions);
+    //check if email sent
+    if (!emailSent) throw new Error("Failed to send OTP");
+
+    //if email sent successfully
+    user.otp = otp;
+    user.otpExpiresAt = Date.now() + 600000;
+    await user.save();
+
+    return { otpExpiresAt: user.otpExpiresAt };
   } catch (error) {
     console.log("Error sending email", error);
     return res.status(500).json({ message: "Server error. Error sending OTP" });
