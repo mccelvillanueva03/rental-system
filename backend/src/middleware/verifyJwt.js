97a3f093bd66d12d557bdf1cb3a0ae84f19babcd
Mccel Villanueva MacOS <mccel.villanueva1103@gmail.com>
2025-09-25 17:55:42 +0800

    feat(auth): implement JWT verification and role-based authorization for change password route

---
+++ backend/src/middleware/verifyJwt.js
@@ -0,0 +1,51 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+export async function verifyToken(req, res, next) {
+  try {
+    const authHeaders = req.headers.authorization;
+
+    if (!authHeaders || !authHeaders.startsWith("Bearer "))
+      return res.status(400).json({ message: "Not Authorized." });
+
+    const token = authHeaders.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) return res.status(404).json({ message: "User Not Found." });
+
+    //check if user changed password after token was issued
+    if (user.passwordChangedAt) {
+      const passwordChangedTimestamp = parseInt(
+        user.passwordChangedAt.getTime() / 1000,
+        10
+      );
+      if (passwordChangedTimestamp > decoded.iat)
+        return res.status(401).json({
+          message: "Password was changed. Please log in again.",
+        });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in verifying token", error);
+    res.status(500).json({ message: "Server Error." });
+  }
+}
+
+export function authorizeRole(...roles) {
+  return (req, res, next) => {
+    // Ensure req.user is set by verifyToken middleware
+    if (!req.user) {
+      return res.status(401).json({ message: "User not authenticated." });
+    }
+    // Check if the user's role is included in the allowed roles
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access Denied." });
+    }
+    // If the role is authorized, proceed to the next middleware
+    next();
+  };
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-    changePassword,
+  changePassword,
   forgotPassword,
   getAllUsers,
   login,
@@ -9,16 +9,24 @@ import {
   verifyEmail,
   verifyForgotPassword,
 } from "../controllers/authController.js";
+import { authorizeRole, verifyToken } from "../middleware/verifyJwt.js";
 
 const router = express.Router();
 
+//protected routes
 router.get("/users", getAllUsers);
+router.post(
+  "/change-password",
+  verifyToken,
+  authorizeRole("tenant", "host"),
+  changePassword
+);
+//public routes
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/verify-email-otp", verifyEmail);
 router.post("/resend-email-otp", resendOTP);
 router.post("/forgot-password", forgotPassword);
 router.post("/verify-forgot-password", verifyForgotPassword);
-router.post("/change-password", changePassword);
 
 export default router;
