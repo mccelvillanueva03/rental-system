dae6c1dfa70909178fb2407a70ecaa80aa946e21
Mccel Villanueva MacOS <mccel.villanueva1103@gmail.com>
2025-10-03 23:15:55 +0800

    feat(refreshToken): implement refresh token functionality with user validation and error handling

---
+++ backend/src/controllers/authentication/refreshToken.js
@@ -0,0 +1,56 @@
+import User from "../../models/User.js";
+import { signToken } from "../../utils/signToken.js";
+
+// Cookie options
+export const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+};
+
+export async function refreshToken(req, res) {
+  try {
+    // Read refresh token from cookie
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(401).json({ message: "No refresh token provided." });
+    }
+    // Find user by refresh token
+    const user = await User.findOne({ refreshToken }).select(
+      "+refreshToken +refreshTokenExpiresAt"
+    );
+    if (!user) return res.status(404).json({ message: "User Not Found." });
+
+    //compare refreshToken to user db hashed refreshToken
+    if (!user.refreshToken || user.refreshToken !== refreshToken) {
+      return res.status(403).json({ message: "Invalid Token" });
+    }
+    if (user.refreshTokenExpiresAt <= Date.now()) {
+      user.refreshToken = null;
+      user.refreshTokenExpiresAt = null;
+      await user.save();
+      return res
+        .status(403)
+        .json({ message: "Refresh Token expired. Please login again." });
+    }
+
+    const { accessToken, newRefreshToken, refreshTokenExpiresAt } =
+      signToken(user);
+    user.refreshToken = newRefreshToken;
+    user.refreshTokenExpiresAt = refreshTokenExpiresAt;
+    await user.save();
+    const userSafe = user.toObject();
+    delete userSafe.password;
+    delete userSafe.refreshToken;
+
+    return res
+      .cookie("refreshToken", newRefreshToken, cookieOptions)
+      .status(200)
+      .json({ accessToken, user: userSafe });
+  } catch (error) {
+    console.error("Error in refreshToken:", error);
+    return res.status(500).json({ message: "Server error." });
+  }
+}
